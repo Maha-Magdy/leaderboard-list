409f64605c138ff9e2eb8859f4b2c7553052a6a3
Maha Magdy <mahamagdy3@gmail.com>
2021-09-10 00:19:04 +0200

    Implement the form 'Submit' button (sending data to the API)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-plusplus */
-import './css/style.css';
-// import NewRecord from './new-record.js';
-import RecentScores from './recent-scores.js';
+import "./css/style.css";
+import NewRecord from "./new-record.js";
+import RecentScores from "./recent-scores.js";
 
 function getRecentScores() {
-  const recentScoresList = document.getElementById('recent-scores-list');
-  const ul = document.createElement('ul');
+  const recentScoresList = document.getElementById("recent-scores-list");
+  const ul = document.createElement("ul");
   const recentScores = RecentScores.getRecentScores();
   for (let i = 0; i < recentScores.length; i++) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.appendChild(
       document.createTextNode(
-        `${recentScores[i].name}: ${recentScores[i].score}`,
-      ),
+        `${recentScores[i].name}: ${recentScores[i].score}`
+      )
     );
     ul.appendChild(li);
   }
@@ -20,4 +20,19 @@ function getRecentScores() {
   recentScoresList.appendChild(ul);
 }
 
-window.addEventListener('load', getRecentScores());
+window.addEventListener("load", getRecentScores());
+
+const name = document.getElementById("name");
+const score = document.getElementById("score");
+const submitNewScore = document.getElementById("submit-new-score");
+
+submitNewScore.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (name.value !== "" && score.value !== "") {
+    const newRecord = new NewRecord(name.value, score.value);
+    newRecord.addNewRecord();
+    name.value = null;
+    score.value = null;
+  }
+});
--- src/new-record.js
+++ src/new-record.js
@@ -3,4 +3,22 @@ export default class NewRecord {
     this.name = name;
     this.score = score;
   }
+
+  async addNewRecord() {
+    const response = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdz/scores/",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ user: this.name, score: this.score }),
+      }
+    );
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      alert("There is an error:", error);
+    }
+  }
 }
